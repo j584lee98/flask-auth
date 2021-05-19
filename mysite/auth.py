bc35ab26773bb78d81ed88c4a2a5a0543c744171
j584lee98 <57649385+j584lee98@users.noreply.github.com>
2021-05-19 18:17:00 -0400

    Tweak login and signup forms

--- mysite/auth.py
+++ mysite/auth.py
@@ -1,11 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
   return render_template('login.html')
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+  email = request.form.get('email')
+  password1 = request.form.get('password1')
+  password2 = request.form.get('password2')
+  
   return render_template('signup.html')
