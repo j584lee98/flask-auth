3f8511cac1e25a73c1de2791b691631ba919814f
j584lee98 <57649385+j584lee98@users.noreply.github.com>
2021-05-20 11:06:49 -0400

    Set up login/logout session feature

--- mysite/__init__.py
+++ mysite/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv, find_dotenv
+from flask_login import LoginManager
 
 load_dotenv(find_dotenv())
 
@@ -28,5 +29,13 @@ def create_app():
 
   with app.app_context():
     db.create_all()
+
+  login_manager = LoginManager()
+  login_manager.login_view = 'auth.login'
+  login_manager.init_app(app)
+
+  @login_manager.user_loader
+  def load_user(id):
+    return User.query.get(int(id))
 
   return app
--- mysite/auth.py
+++ mysite/auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required, current_user
 
 from .models import User
 from . import db
@@ -16,13 +17,21 @@ def login():
     
     if user:
       if check_password_hash(user.password, password):
+        login_user(user, remember=True)
         flash('Logged in successfully.', category='success')
+        return redirect(url_for('views.home'))
       else:
         flash('Incorrect email or password.', category='error')
     else:
       flash('Incorrect email or password.', category='error')
   
   return render_template('login.html')
+
+@auth.route('/logout')
+@login_required
+def logoout():
+  logout_user()
+  return redirect(url_for('auth.login'))
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -49,6 +58,7 @@ def signup():
       new_user = User(first_name=first_name, last_name=last_name, email=email, password=generate_password_hash(password1, method='sha256'))
       db.session.add(new_user)
       db.session.commit()
+      login_user(user, remember=True)
       flash('Account created successfully.', category='success')
       return redirect(url_for('views.home'))
 
