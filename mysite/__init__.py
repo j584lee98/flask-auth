5d35e617840b718457cc4a5992cd0576d9f5e90d
j584lee98 <57649385+j584lee98@users.noreply.github.com>
2021-05-19 20:39:16 -0400

    Set up postgres with db uri

--- mysite/__init__.py
+++ mysite/__init__.py
@@ -1,8 +1,18 @@
+import os
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+db = SQLAlchemy()
 
 def create_app():
   app = Flask(__name__)
   app.config['SECRET_KEY'] = 'flask_app_secret'
+  app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('POSTGRES_URI')
+  db.init_app(app)
 
   from .views import views
   from .auth import auth
