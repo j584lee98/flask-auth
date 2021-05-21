08b628653ba4c675710c466fcb1b73efb97ba146
j584lee98 <57649385+j584lee98@users.noreply.github.com>
2021-05-20 21:10:23 -0400

    Add new todo functionality with todo list display

--- mysite/views.py
+++ mysite/views.py
@@ -1,9 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
+
+from . import db
+from .models import Todo
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+  if request.method == 'POST':
+    todo = request.form.get('todo')
+    new_todo = Todo(todo=todo, user_id=current_user.id)
+    db.session.add(new_todo)
+    db.session.commit()
+    flash('Todo created successfully!', category='success')
+
   return render_template("home.html")
