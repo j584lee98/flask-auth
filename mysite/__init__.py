b5a62079940a3e98725242dbd2445ef535e6b30b
j584lee98 <57649385+j584lee98@users.noreply.github.com>
2021-05-21 14:12:58 -0400

    Remove dotenv library and update __init__

--- mysite/__init__.py
+++ mysite/__init__.py
@@ -6,14 +6,12 @@ from flask_migrate import Migrate
 from dotenv import load_dotenv, find_dotenv
 from flask_login import LoginManager
 
-load_dotenv(find_dotenv())
-
 db = SQLAlchemy()
 
 def create_app():
   app = Flask(__name__)
-  app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
-  app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('POSTGRES_URI')
+  app.config['SECRET_KEY'] = os.environ['FLASK_SECRET_KEY']
+  app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['POSTGRES_URI']
   db.init_app(app)
 
   migrate = Migrate(app, db)
